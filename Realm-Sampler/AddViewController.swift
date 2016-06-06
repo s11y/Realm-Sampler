377a5f3b5035a9d089bb4fb064b476e5cdb1ee5b
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-06-07 00:52:19 +0900

    [Create] AddViewController

---
+++ Realm-Sampler/AddViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddViewController.swift
+//  Realm-Sampler
+//
+//  Created by ShinokiRyosei on 2016/06/07.
+//  Copyright © 2016年 ShinokiRyosei. All rights reserved.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Realm-Sampler/ToDoModel.swift
+++ Realm-Sampler/ToDoModel.swift
@@ -10,10 +10,43 @@ import UIKit
 import RealmSwift
 
 class ToDoModel: Object {
-    dynamic var id: Int = 0
+    
+    static let realm = try! Realm()
+    
+    dynamic private var id: Int = 0
     dynamic var todo: String = ""
     dynamic var category: Int = 0
     dynamic var due_date: NSDate!
-//    dynamic var 
     
+    override static func primaryKey() -> String {
+        return "id"
+    }
+    
+    static func create() -> ToDoModel {
+        let todo = ToDoModel()
+        return todo
+    }
+    
+    static func loadAll() -> [ToDoModel] {
+        let todos = realm.objects(ToDoModel).sorted("id", ascending: false)
+        var ret: [ToDoModel] = []
+        for todo in todos {
+            ret.append(todo)
+        }
+        return ret
+    }
+    
+    static func lastId() -> Int {
+        if let todo = realm.objects(ToDoModel).last {
+            return todo.id + 1
+        }else {
+            return 1
+        }
+    }
+    
+    func save() {
+        try! ToDoModel.realm.write {
+            ToDoModel.realm.add(self)
+        }
+    }
 }
