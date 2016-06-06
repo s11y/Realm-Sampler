1e961020decaf6a81d25246e3b0d69f2f2651589
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-06-06 22:59:23 +0900

    initial commit

---
+++ Realm-Sampler/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Realm-Sampler
+//
+//  Created by ShinokiRyosei on 2016/06/06.
+//  Copyright © 2016年 ShinokiRyosei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
