96b529b8a0b2917ac81c4a773426fe9f4d653deb
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-06-14 00:30:00 +0900

    [Create] AddViewExtension

---
+++ Realm-Sampler/AddViewExtension.swift
@@ -0,0 +1,16 @@
+//
+//  AddViewExtension.swift
+//  Realm-Sampler
+//
+//  Created by ShinokiRyosei on 2016/06/14.
+//  Copyright © 2016年 ShinokiRyosei. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension AddViewController {
+    func setDatePicker() {
+        
+    }
+}
