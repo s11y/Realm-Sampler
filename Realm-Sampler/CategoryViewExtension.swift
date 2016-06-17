6fce5d265ae1aaa9e3094b9624ffbde9d197020f
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-06-18 03:36:20 +0900

    [Fix] delete cell bug

--- Realm-Sampler/CategoryViewExtension.swift
+++ Realm-Sampler/CategoryViewExtension.swift
@@ -10,6 +10,14 @@ import Foundation
 import UIKit
 
 extension CategoryViewController {
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+    }
+    
+    func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return categories.count
     }
@@ -22,11 +30,9 @@ extension CategoryViewController {
         return cell
     }
     
-    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-    }
-    
     func tableView(tableView: UITableView, editActionsForRowAtIndexPath indexPath: NSIndexPath) -> [UITableViewRowAction]? {
         let delete = UITableViewRowAction(style: .Normal, title: "Delete") { (action, index) in
+            self.categories.removeAtIndex(indexPath.row)
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
             self.deleteModel(index: indexPath.row)
         }
