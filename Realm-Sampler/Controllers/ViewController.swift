60958ae4feef31c4987dd21d7a0dcfd2ba87b072
ShinokiRyosei <shinoki.r.0301@gmail.com>
2018-09-25 14:24:43 +0900

    refactor

--- Realm-Sampler/Controllers/ViewController.swift
+++ Realm-Sampler/Controllers/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     
     let realm = try! Realm()
     
-    @IBOutlet var todoTable: UITableView!
+    @IBOutlet var tableView: UITableView!
     
     @IBOutlet var segment: UISegmentedControl! // 全件取得か未完了ToDoかどちらを表示するか決めるためのUISegmentControl
     
@@ -25,10 +25,10 @@ class ViewController: UIViewController {
         
         super.viewDidLoad()
         
-        todoTable.delegate = self
-        todoTable.dataSource = self
+        tableView.delegate = self
+        tableView.dataSource = self
         
-        todoTable.register(TodoCell.self)
+        tableView.register(TodoCell.self)
         
         segment.addTarget(self, action: #selector(self.changeSegment(_:)), for: .touchUpInside) // UISegmentControlのtargetを指定
         
@@ -45,15 +45,15 @@ class ViewController: UIViewController {
         
         self.read()
         
-        todoTable.estimatedRowHeight = 90
-        todoTable.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 90
+        tableView.rowHeight = UITableView.automaticDimension
     }
     
     // 画面が表示される度に、Realmからデータを全件取得し、表示
     func read() {
         todos = ToDoModel.loadUndone()
         segment.selectedSegmentIndex = 0
-        todoTable.reloadData()
+        tableView.reloadData()
     }
     
     // Addボタンを押したときの処理(画面遷移)
@@ -83,7 +83,7 @@ class ViewController: UIViewController {
         default:
             break
         }
-        todoTable.reloadData()
+        tableView.reloadData()
     }
     
     // 初回起動時に、Realmにカテゴリーをセット
@@ -94,5 +94,73 @@ class ViewController: UIViewController {
             category.save()
         }
     }
+
+}
+extension ViewController: UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+
+    }
+
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        // スワイプでDeleteボタンを表示
+        let delete = UITableViewRowAction(style: .normal, title: "Delete") { (action, indexPath) in
+            tableView.isEditing = false
+            // 該当のデータを定数に代入
+            let item = self.realm.objects(ToDoModel.self)[indexPath.row]
+            // 該当のデータをデータベースを削除する
+            try! self.realm.write {
+                self.realm.delete(item
+                )
+            }
+            // UITableViewから削除する
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+
+        delete.backgroundColor = UIColor.red
+
+        // スワイプでDoneボタンを表示
+        let done = UITableViewRowAction(style: .normal, title: "DONE") { (action, indexPath) in
+            // 該当のデータを定数に代入
+            let item = self.realm.objects(ToDoModel.self)[indexPath.row]
+            //データベースのデータを更新する
+            try! self.realm.write({
+                item.isDone = 1 // isDoneの値を1に変更
+            })
+        }
+
+        done.backgroundColor = UIColor.green
+
+        // スワイプでEditボタンを表示
+        let edit = UITableViewRowAction(style: .normal, title: "Edit") { (action, indexpath) in
+            // AddViewに渡すデータを代入
+            self.todo = self.todos[indexPath.row]
+            self.transition() // 画面遷移
+        }
+
+        return [delete, done, edit]
+    }
 }
 
+
+extension ViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todos.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let cell: TodoCell = tableView.dequeueReusableCell(forIndexPath: indexPath)
+
+        let item = todos[indexPath.row]
+        cell.duedateLabel.text = item.due_date.convertDate()
+        cell.todoLabel.text = item.todo
+
+        return cell
+    }
+}
