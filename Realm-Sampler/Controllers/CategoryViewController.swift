5dd56c664eb0ab729a4e8fc844de88760a8d760e
ShinokiRyosei <shinoki.r.0301@gmail.com>
2018-09-25 14:21:57 +0900

    refactor
    extensionが別ファイルで見通しが悪かったので、同じファイルに移動。

--- Realm-Sampler/Controllers/CategoryViewController.swift
+++ Realm-Sampler/Controllers/CategoryViewController.swift
@@ -11,7 +11,7 @@ import RealmSwift
 
 class CategoryViewController: UIViewController {
     
-    @IBOutlet var categoryTable: UITableView! // CategoryModelのデータを表示するためのTableView
+    @IBOutlet var tableView: UITableView! // CategoryModelのデータを表示するためのTableView
     
     var categories: [CategoryModel] = [] // TableViewで表示する配列
     
@@ -22,10 +22,10 @@ class CategoryViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        categoryTable.register(CategoryCell.self)
+        tableView.register(CategoryCell.self)
         
-        categoryTable.delegate = self
-        categoryTable.dataSource = self
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -42,7 +42,7 @@ class CategoryViewController: UIViewController {
     // CategoryModelを全件取得する
     func read() {
         categories = CategoryModel.loadAll()
-        categoryTable.reloadData()
+        tableView.reloadData()
     }
     
     // 該当のCategoryを削除する
@@ -64,3 +64,52 @@ class CategoryViewController: UIViewController {
         }
     }
 }
+
+extension CategoryViewController: UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        // Swipeの時の、Deleteボタン
+        let delete = UITableViewRowAction(style: .normal, title: "Delete") { (action, index) in
+            self.categories.remove(at: indexPath.row) // 配列から削除
+            tableView.deleteRows(at: [indexPath as IndexPath], with: .fade) // TableViewから削除
+            self.deleteModel(index: indexPath.row) // 該当のデータをデータベースから削除
+        }
+
+        delete.backgroundColor = UIColor.red
+
+        // Swipeの時の、Editボタン
+        let edit = UITableViewRowAction(style: .normal, title: "Edit") { (action, index) in
+            self.updatingCategory = self.categories[index.row] // データを保存するための変数に代入
+            self.transition() // 画面遷移
+        }
+
+        return [delete, edit]
+    }
+
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+
+    }
+
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+
+
+}
+
+
+extension CategoryViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: CategoryCell = tableView.dequeueReusableCell(forIndexPath: indexPath)
+
+        cell.categoryLabel.text = categories[indexPath.row].category
+
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.count
+    }
+}
+
