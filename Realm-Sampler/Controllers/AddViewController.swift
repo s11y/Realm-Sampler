4ad425366a2eea12bc0b0e4254575a17184a53d1
ShinokiRyosei <shinoki.r.0301@gmail.com>
2018-09-25 14:23:06 +0900

    refactor

--- Realm-Sampler/Controllers/AddViewController.swift
+++ Realm-Sampler/Controllers/AddViewController.swift
@@ -125,3 +125,65 @@ class AddViewController: UIViewController {
         self.navigationController?.popViewController(animated: true)
     }
 }
+
+extension AddViewController: UIPickerViewDelegate  {
+    // UIDatePickerをdateTextFieldを追加
+    func setDatePicker() {
+        datePicker = UIDatePicker()
+        datePicker.addTarget(self, action: #selector(self.changedDueDate), for: .valueChanged)
+        datePicker.datePickerMode = UIDatePicker.Mode.dateAndTime
+
+        dateTextField.inputView = datePicker
+    }
+
+    // UIPickerViewをcategoryTextFieldを追加
+    func setCategoryPicker() {
+        categoryPicer = UIPickerView()
+        categoryPicer.delegate = self
+        categoryPicer.dataSource = self
+
+        categoryTextField.inputView = categoryPicer
+    }
+
+    // 画面上のUIGestureRecognizerのTargetを決める
+    func setGestureSeletor() {
+        gestureRecognizer.addTarget(self, action: #selector(self.didSelectTapGesture))
+    }
+
+    // 画面を触った時に、キーボードを下げる
+    @objc func didSelectTapGesture() {
+        dateTextField.resignFirstResponder()
+        categoryTextField.resignFirstResponder()
+    }
+
+
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.convertCategory(selectedRow: row)
+        self.category = categoryArray[row]
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return categoryArray[row].category
+    }
+}
+
+extension AddViewController: UIPickerViewDataSource {
+
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return categoryArray.count
+    }
+}
+
+
+extension AddViewController: UITextFieldDelegate {
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
+
